79be493e9486b6c7f78dd38f8ae0b4a0b78a9e7c
asma-pathan <asmapathan752@gmail.com>
2022-11-30 12:05:00 +0530

    props

---
+++ src/ClassComponent.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class ClassComponent extends React.Component{
+	render(){
+		return(
+			<div>
+			from class Componet
+			</div>
+
+			)
+	}
+	
+}
+export default ClassComponent
---
+++ src/Demo.js
@@ -0,0 +1,28 @@
+/*import ClassComponent from './ClassComponent'
+import ClassComponent2 from './ClassComponent2'
+
+function Demo(){
+	return (
+		<section>
+		 <ClassComponent/>
+		 <ClassComponent2/>	
+         <p>enjoy pandagow</p>
+         <h1>hello world</h1>
+        </section>
+	)
+}
+export default Demo*/
+
+
+import PropsExample from './PropsExample'
+function Demo(){
+	return(
+       <div> 
+         <h1>this is about props propsExample</h1>
+         <PropsExample name="sumiesh" age="104"/>
+
+       </div>
+
+		)
+}
+export default Demo
